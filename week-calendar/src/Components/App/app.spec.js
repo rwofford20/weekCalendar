190e9af4b62831226ac78641d67fbfa92f7a498c
rachel <rwofford20@my.whitworth.edu>
2020-04-08 11:01:44 -0700

    2 tests that fail

---
+++ week-calendar/src/Components/App/app.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe ('App', () => {
+    let wrapper; 
+
+    beforeEach(() => wrapper = shallow(< App />));
+
+    it('should render a <div />', () => {
+        expect(wrapper.find('div').length).toEqual(1);
+    });
+
+    it('should render correctly', () => {
+        expect(wrapper).toMatchSnapshot()
+    });
+});
